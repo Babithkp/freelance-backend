d83cd26a1226a5b72fc4d2e444e0183b48366111
Babithkp <babithkullachetti44@gmail.com>
2024-04-09 14:12:41 +0530

    small change in card model

--- models/card.js
+++ models/card.js
@@ -17,11 +17,11 @@ const mongoose = require("mongoose");
     cvc:{
         type: Number,
         minlength:[3,"Full cvc cannot be less than 3 characters"]
-    },
-    address:{
-        type: String,
-        required: true
     }
+    // address:{
+    //     type: String,
+    //     required: true
+    // }
 })
 
 module.exports = mongoose.model("Card", UserSchema);
